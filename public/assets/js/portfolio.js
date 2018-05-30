961222f370e48fad38cc9fe4769abc595a4ee589
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-05-30 13:17:40 -0500

    Portfolio updates

---
+++ public/assets/js/portfolio.js
@@ -0,0 +1,34 @@
+var portfolioObj = [
+    {
+        img: "./assets/images/portfolio1.jpeg",
+        name: "portfolio-1",
+        desc: "This will be some description for the portfolio"
+    },
+    {
+        img: "./assets/images/portfolio2.jpeg",
+        name: "portfolio-1",
+        desc: "This will be some description for the portfolio"
+    },
+    {
+        img: "./assets/images/portfolio3.jpeg",
+        name: "portfolio-1",
+        desc: "This will be some description for the portfolio"
+    },
+    {
+        img: "./assets/images/portfolio4.jpeg",
+        name: "portfolio-1",
+        desc: "This will be some description for the portfolio"
+    },
+    {
+        img: "./assets/images/portfolio5.jpeg",
+        name: "portfolio-1",
+        desc: "This will be some description for the portfolio"
+    },
+    {
+        img: "./assets/images/portfolio6.jpeg",
+        name: "portfolio-1",
+        desc: "This will be some description for the portfolio"
+    }
+
+]
+
