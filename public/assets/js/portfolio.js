15bfbdc8a64c215dd2d350c2216371ba66c1494b
Montgomery <Courtney_Montgomery@Dell.com>
2018-08-29 18:04:34 -0500

    Updates
    Changed titles to reflect projects listed; added placehoder images

--- public/assets/js/portfolio.js
+++ public/assets/js/portfolio.js
@@ -1,32 +1,37 @@
 const projects = [
     {
-        img: "./public/assets/images/screenshots/_harkup",
-        title: "portfolio-1",
+        img: "./public/assets/images/screenshots/placeholder.jpeg",
+        title: "Waste Not",
+        caption: "This will be some description for the portfolio. Built using AAA, BBB, CCC"
+    },
+    {
+        img: "./public/assets/images/screenshots/placeholder.jpeg",
+        title: "Hark Up",
         caption: "This will be some description for the portfolio"
     },
     {
-        img: "./images/screenshots/portfolio1.jpeg",
-        title: "portfolio-1",
+        img: "./public/assets/images/screenshots/placeholder.jpeg",
+        title: "Star Wars RPG",
         caption: "This will be some description for the portfolio"
     },
     {
-        img: "./images/screenshots/portfolio1.jpeg",
-        title: "portfolio-1",
+        img: "./public/assets/images/screenshots/placeholder.jpeg",
+        title: "Memory Challenge",
         caption: "This will be some description for the portfolio"
     },
     {
-        img: "./images/screenshots/portfolio1.jpeg",
-        title: "portfolio-1",
+        img: "./public/assets/images/screenshots/placeholder.jpeg",
+        title: "Hangman",
         caption: "This will be some description for the portfolio"
     },
     {
-        img: "./images/screenshots/portfolio1.jpeg",
-        title: "portfolio-1",
+        img: "./public/assets/images/screenshots/placeholder.jpeg",
+        title: "Crystals RPG",
         caption: "This will be some description for the portfolio"
     },
     {
-        img: "./images/screenshots/portfolio1.jpeg",
-        title: "portfolio-1",
+        img: "./public/assets/images/screenshots/placeholder.jpeg",
+        title: "Project 6",
         caption: "This will be some description for the portfolio"
     }
 ]
