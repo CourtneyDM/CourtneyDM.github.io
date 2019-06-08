b7dd435f1de6a8c3e021f302a38f655d9ae2b6eb
Courtney Montgomery <courtdmontgomery@gmail.com>
2019-06-07 22:49:44 -0500

    refactored code, removed unused icons

---
+++ public/assets/js/icons-backend.js
@@ -0,0 +1,30 @@
+const backEndLogos = [
+	{
+		img: "./public/assets/images/icons/npm.svg",
+		name: "Node Package Manager"
+	},
+	{
+		img: "./public/assets/images/icons/nodejs.svg",
+		name: "NodeJS"
+	},
+	{
+		img: "./public/assets/images/icons/expressJS.png",
+		name: "ExpressJS"
+	},
+	{
+		img: "./public/assets/images/icons/mongodb.svg",
+		name: "MongoDB"
+	},
+	{
+		img: "./public/assets/images/icons/mysql.svg",
+		name: "MySQL"
+	},
+	{
+		img: "./public/assets/images/icons/passport.png",
+		name: "PassportJS"
+	},
+	{
+		img: "./public/assets/images/icons/firebase.svg",
+		name: "Google Firebase"
+	}
+];
---
+++ public/assets/js/icons-frontend.js
@@ -0,0 +1,30 @@
+const frontEndLogos = [
+	{
+		img: "./public/assets/images/icons/html5.svg",
+		name: "HTML5"
+	},
+	{
+		img: "./public/assets/images/icons/css3.svg",
+		name: "CSS3"
+	},
+	{
+		img: "./public/assets/images/icons/javascript.svg",
+		name: "Javascript"
+	},
+	{
+		img: "./public/assets/images/icons/jquery.svg",
+		name: "jQuery"
+	},
+	{
+		img: "./public/assets/images/icons/bootstrap4.svg",
+		name: "Bootstrap"
+	},
+	{
+		img: "./public/assets/images/icons/react.svg",
+		name: "ReactJS"
+	},
+	{
+		img: "./public/assets/images/icons/angular.svg",
+		name: "Angular"
+	}
+];
